876de91cc64bbc3f8f39368431345b025993c41d
mcabarros <marcia.c.a.barros@gmail.com>
2019-11-28 15:39:21 +0000

    test2

--- calculator.py
+++ calculator.py
@@ -1,10 +1,66 @@
 def sum(n1, n2):
+    """
+    Add one number to the other
+
+    :param n1: First number
+    :type n1: float
+    :param n2: Second number
+    :type n2: float
+    :return: sum of two arguments
+    :rtype: float
+
+    :Example:
+
+    >>> sum(1, 2)
+    3
+    >>> sum(-1, 100)
+    99
+
+    """
     return n1 + n2
 
 
 def subtract(n1, n2):
+    """
+    Subtract one number to the other
+
+    :param n1: First number
+    :type n1: float
+    :param n2: Second number
+    :type n2: float
+    :return: subtraction of two arguments
+    :rtype: float
+
+    :Example:
+
+    >>> subtract(1, 2)
+    -1
+    >>> subtract(-1, 100)
+    -101
+
+    """
     return n1 - n2
 
- 
+
 def multiply_three(x1,x2,x3):
+    """
+    Multiply three numbers
+
+    :param x1: First number
+    :type x1: float
+    :param x2: Second number
+    :type x2: float
+    :param x3: Third number
+    :type x3: float
+    :return: multiplication
+    :rtype: float
+
+    :Example:
+
+    >>> multiply_three(1, 2,3)
+    6
+    >>> sum(1.1, 1.2, 1.3)
+    1.716
+
+    """
     return x1 * x2 * x3
